14f1723115f57220fb2d4e3a5af18cee4e96ac4f
Lucas PD <lucasdg92@gmail.com>
2013-06-18 23:04:16 -0300

    update

--- js/cinefilia.js
+++ js/cinefilia.js
@@ -49,7 +49,7 @@ $(document).ready(function(){
 
   // Carregar informações dos filmes
   $.ajax({
-    url: 'http://www.students.ic.unicamp.br/~ra108231/cinefilia_supreme_api/movies.json',
+    url: '192.168.0.13/helper-cinefilia/movies.json',
     dataType: 'jsonp',
     jsonpCallback: 'cineffiliamoviescache',
     success: function(data){
@@ -61,7 +61,7 @@ $(document).ready(function(){
   
   // Carregar informações dos cinemas
   $.ajax({
-    url: 'http://www.students.ic.unicamp.br/~ra108231/cinefilia_supreme_api/cinemas.json',
+    url: '192.168.0.13/helper-cinefilia/cinemas.json',
     dataType: 'jsonp',
     jsonpCallback: 'cineffiliacinemascache',
     success: function(data){
